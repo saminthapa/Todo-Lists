a4200c33e8601c7042ebda56b0085010f647a44c
Samin Thapa <136580949+saminthapa@users.noreply.github.com>
2023-12-11 12:29:29 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+function App(){
+
+  const [userInput, setUserInput] = useState("") 
+ const [todos, setTodos] = useState([])
+ const addData= () => {
+  setTodos([...todos, userInput])
+ } 
+  
+
+  return <div>
+    <h1>Todo List :-</h1> <hr/>
+    <input onChange={(event)=> setUserInput(event.target.value)} type="text"></input>
+    <button className="button" onClick={()=> addData()}>Add</button>
+  <ul>
+    {
+      todos.map((todo, index)=> <li key={index}>{todo}</li> )
+    }
+  </ul>
+
+  </div>
+}
+
+export default App;
